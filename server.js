9fda8871d12798d02e6270603a9442d1c897adeb
kaanilkyaz <kaanilkyaz@gmail.com>
2021-07-05 14:34:15 +0300

    latest

--- server.js
+++ server.js
@@ -22,19 +22,18 @@ app.listen(port, () => console.log(`Server up and running on port ${port} !`));
 app.use(passport.initialize());
 require("./config/passport")(passport);
 app.use("/users", users);
-// cron.schedule("* * * * * *", updateScoreOfUserEverySec);
+cron.schedule("* * * * * *", updateScoreOfUserEverySec);
 
-io.on("connection", function (socket) {
-  console.log("a user connected");
+function updateScoreOfUserEverySec(){
+    io.on("connection", function (socket) {
+        console.log("a user connected");
 
-  socket.on("login", function (data) {
-    console.log("a user " + data.userId + " connected");
-    const user = User.find({ _id: data.userId });
-    user.score += Math.floor(Math.random() * 10);
-    user.save();
-  });
+        socket.on("login", function (data) {
+        console.log("a user " + data.userId + " connected");
+        const user = User.find({ _id: data.userId });
+        user.score += Math.floor(Math.random() * 10);
+        user.save();
+        });
+    });
+}
 
-  socket.on("disconnect", function () {
-    console.log("user disconnected");
-  });
-});
